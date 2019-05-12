77d0a89c98ad442874ff0c470be2ad1a380b2b97
hisham marzouk <tophunter82@gmail.com>
2019-05-12 16:27:23 +0200

    update

--- plugin.video.Arabictv/default.py
+++ plugin.video.Arabictv/default.py
@@ -2,15 +2,10 @@
 from xml.dom import minidom
 import urllib
 
-hrmlink = "https://raw.githubusercontent.com/hishammarzouk/HRMTV/master/LOGOS/"
-hrmthumb = "https://raw.githubusercontent.com/hishammarzouk/HRMTV/master/LOGOS/"
+hrmlink = "https://raw.githubusercontent.com/hishammarzouk/HRMTV/master/LOGOS2/"
+hrmthumb = "https://raw.githubusercontent.com/hishammarzouk/HRMTV/master/LOGOS2/"
 Link = "https://raw.githubusercontent.com/hishammarzouk/Ramadan2017/master/" 
-thumb = "https://raw.githubusercontent.com/hishammarzouk/Ramadan2017/master/logos/"
-#Link = "http://localhost:82/"
-#thumb = "http://localhost:82/logos/"
-CHOD = urllib.urlopen(hrmlink+'lebanon.jpg')
-CHODD = minidom.parse(CHOD)
-channels = CHODD.getElementsByTagName('item',) 
+thumb = "https://raw.githubusercontent.com/hishammarzouk/Ramadan2017/master/logos2/"
 
 Allchannes = urllib.urlopen(Link+'allchannels.xml')
 Allchan = minidom.parse(Allchannes)
@@ -48,21 +43,21 @@ plugin = Plugin()
 @plugin.route('/')
 def show_root_menu():
     items = [{'label': _("HRM"),'path': plugin.url_for('show_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/hrm.png?raw=true") },
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/hrm.png?raw=true") },
     {'label': _("برامج مضحكة"),'path': plugin.url_for('de7k_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/funy.png?raw=true") },
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/funy.png?raw=true") },
     {'label': _("مسلسلات عربية"),'path': plugin.url_for('arabic_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/Amus.png?raw=true") },
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/Amus.png?raw=true") },
     {'label': _("مسلسلات تركية"),'path': plugin.url_for('turk_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/Tmus.png?raw=true") },
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/Tmus.png?raw=true") },
 	{'label': _("اغاني عربية"),'path': plugin.url_for('aghani_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/music.png?raw=true") },
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/music.png?raw=true") },
 	{'label': _("برامج اجتماعية"),'path': plugin.url_for('ijtima_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/ijtema.png?raw=true") },
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/ijtema.png?raw=true") },
 	{'label': _("رسوم متحركة"),'path': plugin.url_for('rousum_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/rusum.png?raw=true") },
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/rusum.png?raw=true") },
 	{'label': _("افلام وثائقية"),'path': plugin.url_for('wasak_channels'),
-    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos/wasak.png?raw=true") }
+    'thumbnail':("https://github.com/hishammarzouk/Ramadan2017/blob/master/logos2/wasak.png?raw=true") }
 	
              ]
 
@@ -76,7 +71,7 @@ def show_channels():
     items = [{
         'label':channel.attributes['name'].value,
         'path': channel.attributes['channel'].value,
-        'thumbnail':hrmthumb + channel.attributes['name'].value + ".jpg",
+        'thumbnail':hrmthumb + channel.attributes['channel'].value + ".jpg",
         'is_playable': True,
     } for channel in channels]
     return plugin.finish(items)
@@ -86,7 +81,7 @@ def show_channels():
 def de7k_channels():
     items = [{
         'label':de7kchannel.attributes['name'].value,
-        'thumbnail':thumb+ de7kchannel.attributes['name'].value + ".jpg",
+        'thumbnail':thumb+ de7kchannel.attributes['channel'].value + ".jpg",
         'path': YOUTUBE_URL_list % de7kchannel.attributes['channel'].value,
     } for de7kchannel in de7kchannels]
     return plugin.finish(items)
@@ -96,7 +91,7 @@ def de7k_channels():
 def arabic_channels():
     items = [{
         'label':arabicchannel.attributes['name'].value,
-        'thumbnail':thumb+ arabicchannel.attributes['name'].value + ".jpg",
+        'thumbnail':thumb+ arabicchannel.attributes['channel'].value + ".jpg",
         'path': YOUTUBE_URL_list % arabicchannel.attributes['channel'].value,
     } for arabicchannel in arabicchannels]
     return plugin.finish(items)
@@ -107,7 +102,7 @@ def arabic_channels():
 def turk_channels():
     items = [{
         'label':turkchannel.attributes['name'].value,
-        'thumbnail':thumb+ turkchannel.attributes['name'].value + ".jpg",
+        'thumbnail':thumb+ turkchannel.attributes['channel'].value + ".jpg",
         'path': YOUTUBE_URL_list % turkchannel.attributes['channel'].value,
     } for turkchannel in turkchannels]
     return plugin.finish(items)	
@@ -120,7 +115,7 @@ def turk_channels():
 def aghani_channels():
     items = [{
         'label':aghanichannel.attributes['name'].value,
-        'thumbnail':thumb+ aghanichannel.attributes['name'].value + ".jpg",
+        'thumbnail':thumb+ aghanichannel.attributes['channel'].value + ".jpg",
         'path': YOUTUBE_URL_list % aghanichannel.attributes['channel'].value,
     } for aghanichannel in aghanichannels]
     return plugin.finish(items)	
@@ -131,7 +126,7 @@ def aghani_channels():
 def ijtima_channels():
     items = [{
         'label':ijtimachannel.attributes['name'].value,
-        'thumbnail':thumb+ ijtimachannel.attributes['name'].value + ".jpg",
+        'thumbnail':thumb+ ijtimachannel.attributes['channel'].value + ".jpg",
         'path': YOUTUBE_URL_list % ijtimachannel.attributes['channel'].value,
     } for ijtimachannel in ijtimachannels]
     return plugin.finish(items)	
@@ -142,7 +137,7 @@ def ijtima_channels():
 def rousum_channels():
     items = [{
         'label':rousumchannel.attributes['name'].value,
-        'thumbnail':thumb+ rousumchannel.attributes['name'].value + ".jpg",
+        'thumbnail':thumb+ rousumchannel.attributes['channel'].value + ".jpg",
         'path': YOUTUBE_URL_list % rousumchannel.attributes['channel'].value,
     } for rousumchannel in rousumchannels]
     return plugin.finish(items)	
@@ -152,7 +147,7 @@ def rousum_channels():
 def wasak_channels():
     items = [{
         'label':wasakchannel.attributes['name'].value,
-        'thumbnail':thumb+ wasakchannel.attributes['name'].value + ".jpg",
+        'thumbnail':thumb+ wasakchannel.attributes['channel'].value + ".jpg",
         'path': YOUTUBE_URL_list % wasakchannel.attributes['channel'].value,
     } for wasakchannel in wasakchannels]
     return plugin.finish(items)	
